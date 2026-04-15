b5cdb9e9a098fdc53886a12fc4d0241dbc4b96b2
subham shaw <subham.s@multitvsolution.com>
2026-04-15 13:20:39 +0530

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,11 @@
+
+export default {
+  basePath: 'https://subham-git1999.github.io/blinkit-clone',
+  allowedHosts: [],
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
